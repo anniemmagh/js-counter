c1d2738cc9bf24e413d5fed7f9551885c2bb3fd1
anniemmagh <91937613+anniemmagh@users.noreply.github.com>
2022-06-10 01:43:38 +0400

    changes

--- assets/js.js
+++ assets/js.js
@@ -1,10 +1,18 @@
-var button = document.getElementById("btncounter");
-count=0;
-button.onclick=function(){
-    count+=1;
-    button.innerHTML='Loading:'+count;
-};
-if (count==5){
-    button.innerHTML='Loading:Done';
-}
- 
+var button = document.getElementById("btncounter");;
+count = 0;
+button.addEventListener("click", test = () => {
+    count += 1;
+    button.innerHTML = 'Loading : ' + count;
+
+    if (count == 10) {
+        button.addEventListener("click", test)
+        timer = setInterval(() => {
+            document.getElementById("btncounter").value = count
+            count -= 1;
+            if (count == 0) {
+                clearInterval(timer);
+                button.addEventListener("click", test);
+            }
+        }, 1000)
+    }
+})
